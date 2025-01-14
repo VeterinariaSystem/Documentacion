92c1de5425e849e77d6ef96a73d2559692334c55
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2025-01-14 18:46:37 -0300

    Paquetes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'VeterinariaSystem', // Usually your GitHub org/user name.
-  projectName: 'veterinaria', // Usually your repo name.
+  projectName: 'Documentacion', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
